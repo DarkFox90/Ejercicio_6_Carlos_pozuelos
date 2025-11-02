4714a8290e72afab86110a2ad5347518b2bcf002
DarkFox90 <carlospozuelos3@gmail.com>
2025-11-02 17:09:53 -0600

    terminada clase Equipo

--- Equipo.java
+++ Equipo.java
@@ -1,3 +1,35 @@
-public class Equipo {
-    
+public abstract class Equipo implements java.lang.Comparable<Equipo> {
+    protected int id;
+    protected String nombre;
+    protected double consumo;
+
+    public Equipo(String nombre, int id, double consumo) {
+        this.nombre = nombre;
+        this.id = id;
+        this.consumo = consumo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    @Override
+    public int compareTo(Equipo e) {
+        return Double.compare(this.consumo, e.consumo);
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               " | Nombre: " + nombre +
+               " | Consumo: " + consumo + "W";
+    }
 }
