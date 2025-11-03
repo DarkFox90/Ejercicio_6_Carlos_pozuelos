17e907706fd7e24101146cc23cc88477a16b54ec
DarkFox90 <carlospozuelos3@gmail.com>
2025-11-02 18:09:00 -0600

    comenzada clase estacion

--- Estacion.java
+++ Estacion.java
@@ -1,3 +1,13 @@
-public class Estacion {
+public class Estacion extends Equipo implements IAccionable, IRegistrable, Imedible {
+    private String accion;
+    private String registrar;
+
+    public Estacion(int id, String nombre, double consumo) {
+        super(nombre, id, consumo);
+        this.accion = "";
+        this.registrar = "";
+
+        
+    }
     
 }
