b18029b99bd008ae95617d3a4a5656cc8159852b
DarkFox90 <carlospozuelos3@gmail.com>
2025-11-03 12:08:11 -0600

    terminada clase estación

--- Estacion.java
+++ Estacion.java
@@ -2,12 +2,25 @@ public class Estacion extends Equipo implements IAccionable, IRegistrable, Imedi
     private String accion;
     private String registrar;
 
-    public Estacion(int id, String nombre, double consumo) {
+    public Estacion(int id, String nombre, double consumo, String accion, String registrar) {
         super(nombre, id, consumo);
-        this.accion = "";
-        this.registrar = "";
+        this.accion = accion;
+        this.registrar = registrar; 
+    }
 
-        
+    @Override
+    public String Accion() {
+        return "esta estación realiza la siguiente accion: " + accion;
+    }
+
+    @Override
+    public String mide() {
+        return "Esta estación mide el tamaño del campo";
+    }
+
+    @Override
+    public String registra() {
+        return "Esta estación registra: " + registrar;
     }
     
 }
